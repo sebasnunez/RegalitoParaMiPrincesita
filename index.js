8ce82204a32a4b586c7a8ae19e8b159ceaa817cd
sebasnunez <69332425+sebasnunez@users.noreply.github.com>
2021-06-12 21:10:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -97,3 +97,15 @@ function playvid(){
 }
 function nada(){
 }
+function enterVR(){
+    ir(navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i){
+        document.querySelector('a-scene').enterVR();
+    }
+}
+enterVR();
